90e78d51f2eb9adec94de939ca0297396194c6a0
ajuj-cuelogic <aju.john@cuelogic.co.in>
2015-10-09 18:41:43 +0530

    get all predictions as an array and save it in location prediction

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -15,13 +15,18 @@ class LocationsController < ApplicationController
   end
 
   def edit
+  	@location = Location.find(params[:id])
   end
 
   def create
     @location = Location.new(location_params)
       if @location.save
-      	descriptions = get_api_connection(@location)
-      	create_location_descriptions(descriptions,@location)
+      	get_api_connection(@location)
+      	# new_arr = []
+      	# @description_arr.each do |v|
+      	# 	new_arr << v.first[1]
+      	# end
+      	create_location_descriptions(@description_arr,@location)
       	# https://maps.googleapis.com/maps/api/place/autocomplete/json?input=<@location.name>&types=geocode&key=token 
         redirect_to @location, notice: 'Location was successfully created.'
       else
@@ -30,15 +35,19 @@ class LocationsController < ApplicationController
   end
 
   def get_api_connection(location)
-  	binding.pry
   	token = "#{Rails.application.secrets[:services]}"
   	uri = URI("https://maps.googleapis.com/maps/api/place/autocomplete/json?input="+location.name+"&types=geocode&key="+token)
-		descriptions = JSON.parse(Net::HTTP.get(uri))['predictions'].first.values.first
+		@description_arr = []
+		JSON.parse(Net::HTTP.get(uri))['predictions'].each do |p|
+			@description_arr << p.values.first
+		end
 	end
 
-	def create_location_descriptions(descriptions,location)
-		loc_descr = LocationPrediction.new(description: descriptions,location_id: location.id)
-		loc_descr.save
+	def create_location_descriptions(description_arr,location)
+		description_arr.each do |d|
+			loc_descr = LocationPrediction.new(description: d,location_id: location.id)
+			loc_descr.save
+		end
 	end
 
   def update
@@ -54,6 +63,8 @@ class LocationsController < ApplicationController
   end
 
   def destroy
+  	@location = Location.find(params[:id])
+  	@location.location_predictions.delete_all
     @location.destroy
     respond_to do |format|
       format.html { redirect_to locations_url, notice: 'Location was successfully destroyed.' }
