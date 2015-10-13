9c9dbb94a8ba3e6ed886a0906080a0e311c7cdba
ajuj-cuelogic <aju.john@cuelogic.co.in>
2015-10-13 16:53:10 +0530

    more actions for locaton_controller_test

--- test/controllers/locations_controller_test.rb
+++ test/controllers/locations_controller_test.rb
@@ -15,9 +15,22 @@ class LocationsControllerTest < ActionController::TestCase
 
   test "should create location" do
     assert_difference('Location.count') do
-      post :create, location: @location
+      post :create, location: @location.attributes
     end
 
     assert_redirected_to location_path(assigns(:location))
+  end
+
+  test "should show location" do
+    get :show, id: @location
+    assert_response :success
+  end
+
+  test "should destroy location" do
+    assert_difference('Location.count', -1) do
+      delete :destroy, id: @location
+    end
+
+    assert_redirected_to locations_path
   end
 end
